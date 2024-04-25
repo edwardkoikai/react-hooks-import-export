7099adf4111babd301fb859a32b9bd204a08913b
edwardkoikai <edwardkoikai@gmail.com>
2024-04-25 16:08:36 +0300

    Add import

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,14 @@
 import React from "react";
-import howManyParks from "./parks/howManyParks";
-
+import aDifferentName  from "./parks/howManyParks";
+import * as RMFunctions from "./parks/RockyMountain"
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
+  aDifferentName (); // => "42 parks!"
+ 
 
-  return <h1>Colorado State Parks!</h1>;
+  return <h1>Colorado State Parks!
+    {RMFunctions.trees}
+    {RMFunctions.wildlife()}
+  </h1>;
 }
+
+export default ColoradoStateParks
